b9539bf0ef16ebfef2e9c98053e61660ed592f05
captain-yun <jslovejs182@gmail.com>
2024-06-13 10:17:40 +0900

    deleting user accountwith mongoDB

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -56,13 +56,13 @@ router.put('/change-password', async (req, res) => {
   
   try {
     const user = await User.findOne({ username : req.user.username })
-
-    if (!user || oldPassword != user.password) {
+    
+    if (!user || ( oldPassword != user.password) ) {
       return res.status(401).json({ message: 'Authentication failed' });
     }
   
     user.password = newPassword;
-    const savedUser = await newUser.save();
+    const savedUser = await user.save();
   
     if (!savedUser) {
       throw new Error('User save operation failed');
@@ -73,14 +73,28 @@ router.put('/change-password', async (req, res) => {
   }
 });
 
-router.delete('/delete-account', (req, res) => {
-  const index = users.findIndex(user => user.id === req.session.passport.user);
-  if (index === -1) {
-    return res.status(404).json({ message: 'Account not found' });
+router.delete('/delete-account', async (req, res) => {  
+  try {
+    const user = await User.findOne({ username : req.user.username })
+    
+    if (!user) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    } 
+    await User.deleteOne({ username : req.user.username })
+
+    // 세션 지우기
+    req.logOut((err) => {
+      if (err) {
+        console.error('Error logging out:', err);
+        return res.status(500).json({ message: 'Error logging out' });
+      }
+      res.status(200).json({ message: 'User deleted and logged out successfully' });
+    });
+
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch(err) {
+    res.status(500).send('Internal server error');
   }
-  users.splice(index, 1);
-  req.logout();
-  res.status(200).json({ message: 'Account deleted successfully' });
 });
 
 export default router;

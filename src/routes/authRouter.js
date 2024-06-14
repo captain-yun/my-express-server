65f8785749dd8694fb1eb8317d33370453b4949e
captain-yun <jslovejs182@gmail.com>
2024-06-14 16:46:47 +0900

    refreshToken added

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+import { generateAccessToken, generateRefreshToken } from '../utils/authUtils.js';
 
 const router = Router();
 
@@ -13,25 +14,59 @@ router.post('/login', (req, res, next) => {
     if (!user) {
       return res.status(401).json({ message: 'Authentication failed' });
     }
-    req.logIn(user, { session: false }, (err) => {
+    req.logIn(user, { session: false }, async (err) => {
       if (err) {
         return next(err);
       }
 
-      const accessToken = jwt.sign({ id: user._id, username: user.username }, 'kitri_secret', { expiresIn: '10m'});
+      const accessToken = generateAccessToken(user);
+      const refreshToken = generateRefreshToken(user);
 
-      return res.status(200).json({ accessToken });
+      user.refreshToken = refreshToken;
+      await user.save();
+
+      return res.status(200).json({ accessToken, refreshToken });
+
     });
   })(req, res, next);
 });
 
-router.get('/logout', (req, res) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    res.status(200).json({ message: 'Logged out successfully' });
-  });
+router.post('/refresh-token', async (req, res) => {
+  const { refreshToken } = req.body;
+  if (!refreshToken) return res.sendStatus(401);
+
+  try {
+    const user = await User.findOne({ refreshToken });
+    if (!user) return res.sendStatus(403);
+
+    jwt.verify(refreshToken, 'kitri_secret2', (err, decoded) => {
+      if (err) return res.sendStatus(403);
+
+      const accessToken = generateAccessToken(user);
+      return res.json({ accessToken });
+    })
+
+  } catch (err) {
+    return res.sendStatus(500);
+  }
+})
+
+router.post('/logout', async (req, res) => {
+  const { refreshToken } = req.body;
+  if (!refreshToken) return res.sendStatus(401);
+
+  console.log(refreshToken)
+  try {
+    const user = await User.findOne({ refreshToken });
+    if (!user) return res.sendStatus(403);
+    console.log(user)
+    user.refreshToken = null;
+    await user.save();
+
+    res.status(200).json({ message : 'Logged out successfully'});
+  } catch (err) {
+    return res.sendStatus(500);
+  }
 });
 
 router.post('/join', async (req, res) => {
@@ -74,30 +109,6 @@ router.put('/change-password', async (req, res) => {
   }
 });
 
-<<<<<<< HEAD
-router.delete('/delete-account', async (req, res) => {  
-  try {
-    const user = await User.findOne({ username : req.user.username })
-    
-    if (!user) {
-      return res.status(401).json({ message: 'Authentication failed' });
-    } 
-    await User.deleteOne({ username : req.user.username })
-
-    // 세션 지우기
-    req.logOut((err) => {
-      if (err) {
-        console.error('Error logging out:', err);
-        return res.status(500).json({ message: 'Error logging out' });
-      }
-      res.status(200).json({ message: 'User deleted and logged out successfully' });
-    });
-
-    res.status(200).json({ message: 'User deleted successfully' });
-  } catch(err) {
-    res.status(500).send('Internal server error');
-  }
-=======
 router.delete('/delete-account', async (req, res) => {
   try {
     const user = await User.findOne({ username : req.user.username })
@@ -119,7 +130,7 @@ router.delete('/delete-account', async (req, res) => {
     console.error('Error deleting account:', err);
     res.status(500).send('Internal server error');
   }  
->>>>>>> 920cd55
+
 });
 
 export default router;
